e1548195e13eaebefa577b2c9fdfae9b1ec37ba9
quotica-user <hotrock42@gmail.com>
2025-05-09 13:34:53 +0900

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div style={{ padding: '40px', fontFamily: 'Arial', textAlign: 'center' }}>
+      <h1>Quoticaへようこそ！</h1>
+      <p>これは動作確認用の最小ページです。</p>
+    </div>
+  );
+}
